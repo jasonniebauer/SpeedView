b6437b68daabc73b193d8ee3c25f7d1650b6fa6a
Jason <lookwhatjasondid@gmail.com>
2019-09-03 18:52:58 -0500

    add updates to test github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-'use strict'; 
+'use strict';
+
+readOut = document.getElementById('speed');
+
+function geo_success(position) {
+    readOut.innerHTML(position.coords.speed)
+}
+  
+function geo_error() {
+    alert("Sorry, no position available.");
+}
+
+var geo_options = {
+    enableHighAccuracy: true, 
+    maximumAge        : 30000, 
+    timeout           : 27000
+};
 
 if ("geolocation" in navigator) {
     /* geolocation is available */
+
+    /* assign an ID number that can be used to uniquely identify the
+    requested position watcher */
+    var watchPositionID = navigator.geolocation.watchPosition(
+        geo_success, geo_error, geo_options);
+
+    /* stop watching user's location */
+    // navigator.geolocation.clearWatch(watchID);
 } else {
     /* geolocation IS NOT available */
 }
