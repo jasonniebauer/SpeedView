75602caf09721eb70f2d964507544528ac6d782d
Jason <lookwhatjasondid@gmail.com>
2019-09-03 19:25:34 -0500

    test

--- index.js
+++ index.js
@@ -16,16 +16,24 @@ var geo_options = {
     timeout           : 27000
 };
 
-if ("geolocation" in navigator) {
-    /* geolocation is available */
-
-    /* assign an ID number that can be used to uniquely identify the
-    requested position watcher */
-    var watchPositionID = navigator.geolocation.watchPosition(
-        geo_success, geo_error, geo_options);
-
-    /* stop watching user's location */
-    // navigator.geolocation.clearWatch(watchID);
-} else {
-    /* geolocation IS NOT available */
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        readOut.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
+
+// if ("geolocation" in navigator) {
+//     /* geolocation is available */
+
+//     /* assign an ID number that can be used to uniquely identify the
+//     requested position watcher */
+//     var watchPositionID = navigator.geolocation.watchPosition(
+//         geo_success, geo_error, geo_options);
+
+//     /* stop watching user's location */
+//     // navigator.geolocation.clearWatch(watchID);
+// } else {
+//     /* geolocation IS NOT available */
+// }
